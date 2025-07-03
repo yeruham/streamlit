4e642f3d6330baeaddf44c488ad21eba51e8fe27
YeruhamMendelson <y0583207549@gmail.com>
2025-07-03 12:40:51 +0300

    start worke wite streamlit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+
+movies_data = pd.read_csv('data/movies.csv')
+m =  movies_data.shape[0]
+st.write("shape: ", m, movies_data.shape[1])
+st.write(movies_data.head())
+print(movies_data.duplicated())
+st.write(movies_data.count())
+st.write(movies_data.columns)
