3148210d997f3bc83fd0f73e8d007c3bd825a6d7
YeruhamMendelson <y0583207549@gmail.com>
2025-07-03 16:07:57 +0300

    two plotes

--- main.py
+++ main.py
@@ -2,10 +2,34 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+col1, col2, col3 = st.columns(3)
+
 movies_data = pd.read_csv('data/movies.csv')
-m =  movies_data.shape[0]
-st.write("shape: ", m, movies_data.shape[1])
+
+
+col1.write('**data columns:**')
+col1.write(movies_data.columns)
+col2.write('**data dtype:**')
+col2.write(movies_data.dtypes)
+col3.write('**data describe:**')
+col3.write(movies_data.describe())
+
+st.write('### five lines from movies data:')
 st.write(movies_data.head())
-print(movies_data.duplicated())
-st.write(movies_data.count())
-st.write(movies_data.columns)
+
+st.write('### plots:')
+
+
+fig = plt.figure(figsize = (19, 10))
+plt.bar(movies_data.genre, movies_data.score)
+plt.title('score movies by genre')
+plt.xlabel('genre')
+plt.ylabel('score')
+st.pyplot(fig)
+
+st.write(movies_data.genre.value_counts())
+
+fig = plt.figure(figsize = (19, 10))
+plt.plot(movies_data.genre.value_counts().sort_values())
+plt.title('num movies by genre')
+st.pyplot(fig)
